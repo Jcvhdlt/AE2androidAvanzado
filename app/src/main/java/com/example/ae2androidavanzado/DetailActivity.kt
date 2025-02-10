e3d19feafb75309009857e681398326b6548f99b
Julian Cardozo Vega <151052868+Jcvhdlt@users.noreply.github.com>
2025-02-10 11:17:55 +0100

    update

--- app/src/main/java/com/example/ae2androidavanzado/DetailActivity.kt
+++ app/src/main/java/com/example/ae2androidavanzado/DetailActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.ae2androidavanzado.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
